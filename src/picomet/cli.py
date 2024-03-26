73fbb8d37a41120a08223c39b3174ff196303cfc
Al Mahdi <68297852+almahdi404@users.noreply.github.com>
2024-03-26 12:30:20 +0600

    impr(cli): use a var jinja ext for startproject

--- src/picomet/cli.py
+++ src/picomet/cli.py
@@ -12,15 +12,5 @@ def main():
     args = parser.parse_args()
     if args.startproject:
         import copier
-        from django.core.management.utils import get_random_secret_key
 
-        copier.run_copy((Path(__file__).parent / "template").as_posix())
-        with open(".env") as file:
-            file_content = file.read()
-
-        modified_content = file_content.replace(
-            "SECRET_KEY=", f"SECRET_KEY={get_random_secret_key()}"
-        )
-
-        with open(".env", "w") as file:
-            file.write(modified_content)
+        copier.run_copy((Path(__file__).parent / "template").as_posix(), unsafe=True)
---
+++ src/picomet/copier.py
@@ -0,0 +1,11 @@
+from django.core.management.utils import get_random_secret_key
+from jinja2 import Environment
+from jinja2.ext import Extension
+
+
+class VarsExtension(Extension):
+    """This extension adds required vars for picomet starter."""
+
+    def __init__(self, environment: Environment) -> None:
+        super().__init__(environment)
+        environment.globals["DJ_SECRET_KEY"] = get_random_secret_key()
