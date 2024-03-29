7d253f2373dcc5643e26d66c5a49ec6f51948434
Al Mahdi <68297852+almahdi404@users.noreply.github.com>
2024-03-29 13:21:02 +0600

    example: improve custom user manager

--- example/apps/core/managers.py
+++ example/apps/core/managers.py
@@ -2,27 +2,27 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, password=None, *args, **kwargs):
+    def create_user(self, username, password=None, **extra_fields):
         if not username:
             raise ValueError("Users must have an username")
-        user = self.model(username=username)
+        user = self.model(username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_staffuser(self, username, password=None, *args, **kwargs):
+    def create_staffuser(self, username, password=None, **extra_fields):
         if not username:
             raise ValueError("Users must have an username")
-        user = self.model(username=username)
+        user = self.model(username=username, **extra_fields)
         user.is_staff = True
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, password=None, *args, **kwargs):
+    def create_superuser(self, username, password=None, **extra_fields):
         if not username:
             raise ValueError("Users must have an username")
-        user = self.model(username=username)
+        user = self.model(username=username, **extra_fields)
         user.is_staff = True
         user.is_superuser = True
         user.set_password(password)
