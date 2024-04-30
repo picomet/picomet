0c5f71a296afba8871d6830ddb8338d5546f35c0
Al Mahdi <68297852+almahdi404@users.noreply.github.com>
2024-04-30 13:30:27 +0600

    example: use whitenoise for prod statics

--- example/project/settings/pro.py
+++ example/project/settings/pro.py
@@ -10,6 +10,8 @@ CSRF_COOKIE_SECURE = True
 
 SESSION_COOKIE_SECURE = True
 
+MIDDLEWARE.insert(0, "whitenoise.middleware.WhiteNoiseMiddleware")  # noqa: F405
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
